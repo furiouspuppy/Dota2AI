478875d46a6abc75bda13930d72955b515ef15f3
furiouspuppy <ian.toal83@gmail.com>
2017-02-04 11:52:33 -0700

    Default Mode Override files
    These files are setup to be inactive until they are set up and
    activated, instructions in files

---
+++ bots/ability_item_usage_generic.lua
@@ -0,0 +1,31 @@
+--Generic Ability/Item Template, delete this return to activate, 
+return
+----------------------------------------------------------------------------------------------------
+-- Any of these functions can be uncommented individually
+-- Those functions left commented will use Dota default implementation
+
+--[[
+function ItemUsageThink( )
+	-- body
+end
+]]
+--[[
+function AbilityUsageThink( )
+	-- body
+end
+]]
+--[[
+function CourierUsageThink( )
+	-- body
+end
+]]
+--[[
+function BuybackUsageThink( )
+	-- body
+end
+]]
+--[[
+function AbilityLevelUpThink( )
+	-- body
+end
+]]
---
+++ bots/mode_ward_generic.lua
@@ -0,0 +1,26 @@
+--Generic Mode Template, delete this return to activate, 
+return
+----------------------------------------------------------------------------------------------------
+-- Any of these functions can be uncommented individually
+-- Those functions left commented will use Dota default implementation
+
+--[[
+function GetDesire( )
+	-- body
+end
+]]
+--[[
+function OnStart( )
+	-- body
+end
+]]
+--[[
+function OnEnd( )
+	-- body
+end
+]]
+--[[
+function Think( )
+	-- body
+end
+]]
